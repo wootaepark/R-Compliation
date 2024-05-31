1b387bd9c9a06d615158311ddec3747cd08e405e
wootaepark <pakdal99@gmail.com>
2024-05-31 20:45:36 +0900

    데이터 분석 0차 커밋

--- data_science/ex1.R
+++ data_science/ex1.R
@@ -1,9 +1,20 @@
-
+###########
+# 
+#
+#
+#
+###########
 
 setRepositories(ind = 1:8)
 
-WORK_DIR <- "C:\\DataScience_R_Compliation\\data_science"
+
+WORK_DIR <- "C:\\DataScience_R_Compliation\\data_science\\Rdata"
 setwd(WORK_DIR)
+
+
+
+
+save.image("ex1.Rdata")
 
 
 
