4867dd1b034cdbcbe263d25801ffa1e943554a3a
wootaepark <pakdal99@gmail.com>
2024-06-16 20:37:30 +0900

    add_comment_Q3

--- data_science/Final_exam_24_1/Q3.R
+++ data_science/Final_exam_24_1/Q3.R
@@ -3,7 +3,7 @@
 
 setRepositories(ind = 1:7)
 
-library("data.table")
+library("data.table") # 데이터 불러오기 위함
 library("dplyr")
 library("caret")
 library("class")
@@ -14,3 +14,8 @@ WORK_DIR <- "C:\\DataScience_R_Compliation\\data_science\\Rdata"
 DATA_DIR <-"C:\\DataScience_R_Compliation\\data_science\\data"
 
 setwd(WORK_DIR)
+
+
+
+
+
