b90f3ea7fd3a1b682db255125d6802bb4b620981
wootaepark <pakdal99@gmail.com>
2024-07-20 19:16:30 +0900

    ADsP 10Day temp commit

---
+++ ADsP/Day10.R
@@ -0,0 +1,18 @@
+# 10일차 ADsP 코드
+
+
+setRepositories(ind = 1:8)
+WORK_DIR <- "C:\\DataScience_R_Compliation\\ADsP\\rdata"
+setwd(WORK_DIR)
+load("Day10.Rdata")
+
+
+
+
+data<-c(1:10)
+
+
+
+
+setwd(WORK_DIR)
+save.image("Day10.Rdata")
